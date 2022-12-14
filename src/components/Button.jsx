18817d01dac7846a2d2e6b23bcbbb9a5717c719c
unknown <munajatabdulhafiz18@gmail.com>
2022-12-14 21:16:11 +0700

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import style from "../Style"
+
+const Button = ({ styles }) => {
+  return (
+    <button 
+      type='button' 
+      className={`
+        py-4 px-6 bg-blue-gradient font-poppins font-medium
+        text-[18px] text-primary outline-none ${styles}
+        rounded-[10px] ${style.buttonTransition}
+      `}
+    >
+      Get Started
+    </button>
+  )
+}
+
+export default Button
